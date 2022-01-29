fa340ab0fa7c434711f3afd663f9d290d951114a
David Ferreira da Cunha <david.cunha@dock.tech>
2022-01-28 23:55:28 -0300

    Entrega do exercício

---
+++ src/main/java/com/devsuperior/bds04/config/AuthorizationServerConfig.java
@@ -0,0 +1,59 @@
+package com.devsuperior.bds04.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+// @EnableAuthorizationServer -> Informa que essa é a classe que representa o AuthorizationServer do oauth.
+// É a classe que recebe as credenciais e devolve um token na resposta.
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+
+	@Autowired private BCryptPasswordEncoder   passwordEncoder;
+	@Autowired private JwtAccessTokenConverter accessTokenConverter;
+	@Autowired private JwtTokenStore           tokenStore;
+	@Autowired private AuthenticationManager   authenticationManager;
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		// Define como que vai ser a autenticação e quis serão os dados do cliente.
+
+		clients.inMemory()                                // Define que o processo ocorrerá em memória.
+			.withClient(clientId)                         // ID da aplicação. Serivá para se comunicar com o backend.
+			.secret(passwordEncoder.encode(clientSecret)) // Senha da aplicação.
+			.scopes("read", "write")                      // Define que a aplicação terá acesso de leitura e escrita.
+			.authorizedGrantTypes("password")             // Tipo de acesso/login.
+			.accessTokenValiditySeconds(jwtDuration);     // Tempo de expiração do token (em segundos).
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		endpoints.authenticationManager(authenticationManager) // Define quem vai autorizar e qual será o formato do token.
+			.tokenStore(tokenStore)                            // Define quais serão os objetos responsáveis por processar o token.
+			.accessTokenConverter(accessTokenConverter);
+	}
+}
---
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.devsuperior.bds04.controllers.exceptions;
+
+import java.time.Instant;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// Essa classe serve para não ter que escrever um try..catch em todos os métodos do controlador que precisar tratar uma exceção.
+// Ela irá interceptar as exceções que ocorrem no controlador e vai dar o tratamento adequado para ela.
+// @ControllerAdvice -> Permite que a classe intercept alguma exceção que ocorrer na camada de resource (controlador Rest) e trata a exceção.
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// HttpServletRequest -> Contém todas as informações da requisição.
+	// @ExceptionHandler  -> Define que irá interceptar uma exceção. O parâmetro é o tipo de exceção que deverá ser interceptada.
+	
+	/*@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound( ResourceNotFoundException e, HttpServletRequest request ) {
+		
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database( DatabaseException e, HttpServletRequest request ) {
+		
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}*/
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation( MethodArgumentNotValidException e, HttpServletRequest request ) {
+		
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
+}
